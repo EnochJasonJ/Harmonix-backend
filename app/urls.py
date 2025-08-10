93cf1338a90e63196510bab07c85ffab07b34e36
Enoch Jason J <enochjasonj@Enochs-MacBook-Air.local>
2025-08-10 08:58:10 +0530

    Updated urls.py

--- app/urls.py
+++ app/urls.py
@@ -13,5 +13,6 @@ router.register('users', UserViewSet, basename='user')
 urlpatterns = [
     path('token/',TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/',TokenRefreshView.as_view(), name='token_refresh'),
+    path('hello/', hello, name='hello'),
     path('', include(router.urls)),
 ]
